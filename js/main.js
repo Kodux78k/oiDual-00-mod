6a35d5feda827c0048140babe403b67afabaa6e5
Kodux78k <koduxlive@gmail.com>
2026-02-02 23:00:58 -0300

    Format console log and update import statements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-console.log("Infodose conectado", {"ts":1770079171772,"id":"477fa4b7-c9da-48fe-bf52-76834785def9","meta":{"app":"generated.app"}});
-import src="https://kodux78k.github.io/oiDual-00-mod/js/modules/inline-0.js";
-import src="https://kodux78k.github.io/oiDual-00-mod/js/modules/inline-1.js";
+console.log("Infodose conectado", {
+    "ts": 1770079171772,
+    "id": "477fa4b7-c9da-48fe-bf52-76834785def9",
+    "meta": { "app": "generated.app" }
+});
+
+import "https://kodux78k.github.io/oiDual-00-mod/js/modules/inline-0.js";
+import "https://kodux78k.github.io/oiDual-00-mod/js/modules/inline-1.js";
